b915717c9914f686d779b7b497665633c23bf978
Antoine LE CHAPELAIN <alechapelain@sii.fr>
2020-10-04 18:42:25 +0200

    feat(*): adapt to netflix changes on interface

--- netflix-clean.js
+++ netflix-clean.js
@@ -15,13 +15,25 @@
 
     /**
      * Check if item is rated (positive or negative)
+     * Retrieve from list from store and adapt to use as array/flatten and find matching item to check
      * @param itemId
      * @returns {*|boolean}
      * @private
      */
     function _isItemRated (itemId) {
-        const videos = window.netflix.reactContext.pathEvaluator._root.cache.videos;
-        return videos[itemId] && videos[itemId].userRating.value.userRating !== 0;
+        const lists = window.netflix.reactContext.pathEvaluator._root.cache.lists;
+        const item = Object.keys(lists)
+            .map(listId => listId.includes('$') ? [] : lists[listId])
+            .map(list => typeof list === 'object' && Object.keys(list).map(itemId => itemId.includes('$') ? [] : list[itemId]))
+            .flat(2)
+            .map(item => item.itemSummary && item.itemSummary.value ? item.itemSummary.value : {})
+            .find(item => item.id === itemId)
+
+        return (
+            item &&
+            item.userRating &&
+            item.userRating.userRating !== 0
+        );
     }
 
     /**
@@ -36,7 +48,7 @@
                 link.lastIndexOf('?')
             );
 
-            card.parentNode.style.opacity = _isItemRated(itemId) ?  '0.1' : '1';
+            card.parentNode.style.opacity = _isItemRated(parseInt(itemId)) ?  '0.1' : '1';
         })
     }
 
